54a177b2a811eabcb0ce5cdb9ed00cb34da33314
ETEC Adolpho Berezin <110746209+riquexavier@users.noreply.github.com>
2024-05-03 21:24:02 -0300

    Pedrão dos acesso

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string senha, correct;
+
+Console.WriteLine("Olá, usuário. Por favor, digite sua senha:");
+senha = Console.ReadLine()!;
+
+correct = "1234abcd";
+if (senha == correct)
+{
+    Console.WriteLine("Acesso permitido");
+}
+else
+{
+    Console.WriteLine("Acesso negado");
+}
